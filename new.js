14426a8e8f7b78d8272b7de85f9e71c8a3f19e03
Chinonso Daniels <61415048+Miss-nonso@users.noreply.github.com>
2023-07-28 02:16:05 +0100

    Add files via upload
    Added some validations

--- new.js
+++ new.js
@@ -1,4 +1,4 @@
-let allInfo = [];
+let database = [];
 
 const time = () => {
   if (new Date().getHours() < 12) {
@@ -8,150 +8,213 @@ const time = () => {
   }
 };
 
-// CREATE ONE
+const autoAdd = () => {
+  database = database.map((user, index) => {
+    let idGenerator = ("USERSC00" + (index + 1)).toString();
+    return {
+      id: idGenerator,
+      userName: user.name + "00" + (index + 1),
+      ...user,
+      status: true,
+      timeCreated: time(),
+      timeUpdated: time(),
+    };
+  });
 
-const createOne = (obj) => {
-  allInfo.push(obj);
-  return allInfo;
+  return database;
 };
 
-createOne({
-  Id: 101,
-  name: "Chioma",
-  active: true,
-  roles: ["driver", "waiter", "CEO"],
-  age: 39,
-  userName: "chiomaUnique27",
-  timeCreated: time(),
-});
+// CREATE ONE
+
+const createOne = (user) => {
+  if (typeof user !== "object") {
+    return "Invalid Entry";
+  } else if (typeof user === "object") {
+    if (user.name && user.role && user.age) {
+      database.push(user);
+    } else {
+      return "One or more field(s) missing. Please Enter name, age and role.";
+    }
+  }
+  return autoAdd();
+};
+
+createOne({ name: "Helen", role: ["Director", "Manager"], age: 76 });
 
 // CREATE MANY
 
-const createMany = (arrofObj) => {
-  arrofObj.map((item) => allInfo.push(item));
-  return allInfo;
+const createMany = (arrOfUsers) => {
+  arrOfUsers.map((user) => {
+    if (typeof user !== "object") {
+      return "Invalid Data type exists in your list";
+    } else {
+      if (user.name && user.role && user.age) {
+        database.push(user);
+        return database;
+      } else {
+        return "Error with entries";
+      }
+    }
+  });
+  return autoAdd();
 };
 
 createMany([
   {
-    Id: 102,
     name: "Onyinye",
-    active: true,
-    roles: ["Engineer", "Server"],
+    role: ["Engineer", "Server"],
     age: 29,
-    userName: "Onyi5050",
-    timeCreated: time(),
   },
   {
-    Id: 103,
     name: "Joan",
-    active: false,
-    roles: ["manager", "Artist"],
+    role: ["manager", "Artist"],
     age: 14,
-    userName: "Joan2039",
-    timeCreated: time(),
   },
   {
-    Id: 104,
     name: "Dolapo",
-    active: false,
-    roles: ["Coordinator", "Director"],
+    role: ["Coordinator", "Director"],
     age: 43,
-    userName: "DollyDollars001",
-    timeCreated: time(),
   },
 ]);
 
 //  GET ONE
 
 const getOne = (id) => {
-  let found = allInfo.find((item) => item.Id == id);
-  return found == undefined ? "This Id does not exist" : found;
+  if (typeof id !== "string") {
+    return "Your input is of an incorrect datatype. Kindly use an appropriate string.";
+  } else {
+    if (
+      id.substring(0, 6).toUpperCase() !== "USERSC" ||
+      isNaN(Number(id.substring(6, id.length)))
+    ) {
+      return "Invalid Id number";
+    } else {
+      let getData = database.find((data) => data.id === id.toUpperCase());
+      if (getData) {
+        return getData;
+      } else {
+        return "No user with such ID";
+      }
+    }
+  }
 };
-
-getOne(109);
+getOne("USERSC002");
 
 // GET MANY
 
 const getMany = (arrOfId) => {
-  let newArr = [];
-  arrOfId.forEach((id) => {
-    const res = allInfo.filter((data) => data.Id == id);
-    if (res.length == 0) {
-      newArr.push(id + " does not exist");
+  let arrOfUsers = [];
+  arrOfId.forEach((data) => {
+    const databasefilter = database.filter((user) => user.id == data);
+    if (typeof data !== "string") {
+      arrOfUsers.push(
+        data + "has an incorrect datatype. Kindly use an appropriate string."
+      );
     } else {
-      newArr.push(res[0]);
+      if (
+        data.substring(0, 6).toUpperCase() !== "USERSC" ||
+        isNaN(Number(data.substring(6, data.length)))
+      ) {
+        arrOfUsers.push(data + " is an Invalid Id number");
+      } else {
+        databasefilter.forEach((filteredEl) => {
+          arrOfUsers.push(filteredEl);
+        });
+      }
     }
   });
-
-  return newArr;
+  return arrOfUsers;
 };
 
-getMany([101, 109]);
+getMany(["USE0SC001", "USERSC002"]);
 
 // UPDATE ONE
 
 const updateOne = (id, val) => {
-  let update = allInfo.map((data) => {
-    if (data.Id == id) {
-      return { ...data, name: val, timeCreated: time() };
+  if (typeof val !== "string") {
+    return "Error. Please use valid strings for both entries";
+  } else {
+    if (
+      typeof id !== "string" ||
+      id.substring(0, 6).toUpperCase() !== "USERSC" ||
+      isNaN(Number(id.substring(6, id.length)))
+    ) {
+      return "Invalid Id number";
+    } else {
+      let UpdatedDatabase = database.map((user) => {
+        return user.id == id
+          ? {
+              ...user,
+              userName: val + id.substring(6, id.length),
+              name: val,
+              timeUpdated: time(),
+            }
+          : user;
+      });
+      return UpdatedDatabase;
     }
-    return data;
-  });
-  return update;
+  }
 };
 
-updateOne(103, "Ashley");
+updateOne("USERSC002", "Jerry");
 
 // UPDATE MANY
 
-const updateMany = (arrOfInfoToChg) => {
-  let result = [];
-  arrOfInfoToChg.forEach((el) => {
-    const user = allInfo.find((data) => data.Id == el[0]);
-    if (!user) {
-      result.push(`User with ${el[0]} not found`);
+const updateMany = (arrOfUpdates) => {
+  let updateValidator = arrOfUpdates.map((data) => {
+    if (typeof data[0] !== "string" || typeof data[1] !== "string") {
+      return data + "  This entry contains invalid datatype(s)";
     } else {
-      allInfo.map((data) => {
-        if (data.Id == el[0]) {
-          const updated = {
-            ...data,
-            name: el[1],
-            userName: el[2],
-            timeUpdated: time(),
-          };
-          result.push(updated);
-          return updated;
-        }
-        return data;
-      });
+      if (
+        data[0].substring(0, 6).toUpperCase() !== "USERSC" ||
+        isNaN(Number(data[0].substring(6, data[0].length)))
+      ) {
+        return data[0] + " This Id is invalid";
+      } else {
+        let updateApplier = database.map((user) => {
+          if (user.id === data[0]) {
+            return {
+              ...user,
+              userName: data[1] + data[0].substring(6, data[0].length),
+              name: data[1],
+              timeUpdated: time(),
+            };
+          } else {
+            return data;
+          }
+        });
+        return updateApplier;
+      }
     }
   });
-  return result;
+  return updateValidator;
 };
 
-updateMany([
-  [101, "Vicky", "Vicky001"],
-  [109, "Beccky", "BecckyBee"],
-  [104, "Jamine", "ClassyJasmine"],
-]);
+console.log(
+  "updateMany",
+  updateMany([
+    ["USERSC001", "Vicky"],
+    ["USERSC002", "Amanda"],
+    ["USERSC003", "Jamine"],
+  ])
+);
 
 // DELETE ONE
 
 const deleteOne = (id) => {
-  let deleter = allInfo.filter((item) => item.Id !== id);
+  let deleter = database.filter((item) => item.Id !== id);
   return deleter == undefined ? "Id does not exist" : deleter;
 };
 
-deleteOne(104);
+deleteOne("USERSC001");
 
 // DELETE MANY
 
 const deleteMany = (arrOfIds) => {
   arrOfIds.map(
-    (item) => (allInfo = allInfo.filter((data) => data.Id !== item))
+    (item) => (database = database.filter((data) => data.Id !== item))
   );
-  return allInfo;
+  return database;
 };
 
 deleteMany([101, 104]);
@@ -159,7 +222,7 @@ deleteMany([101, 104]);
 // RETURN BY NAME
 
 const returnByName = (userName) => {
-  return allInfo.find((item) => item.userName == userName);
+  return database.find((item) => item.userName == userName);
 };
 
-returnByName("Joan2039");
+returnByName("USERSC004");
